2e2ed3aa9699e8f07637e90df9ff4c71bba4c38d
Hriddhiman <69643087+Hriddhiman17@users.noreply.github.com>
2020-10-25 13:14:27 +0530

    Add files via upload

---
+++ Throw.js
@@ -0,0 +1,30 @@
+class Throw{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.pointB= pointB
+        this.throw = Constraint.create(options);
+        World.add(world, this.throw);
+    }
+        fly() {
+         this.throw.bodyA = null
+        }
+        attach(body){
+            this.sling.bodyA = body;
+        }
+        
+    display(){
+        if(this.throw.bodyA){
+            
+       var pointA = this.throw.bodyA.position;
+        var pointB = this.pointB;
+        strokeWeight(4);
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+    }
+    
+}
+}
